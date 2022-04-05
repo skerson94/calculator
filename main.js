68bd83d00147b79cc6674417f09bcf4a3877047c
Sarah Kerson <sarah.kerson25@journalism.cuny.edu>
2022-04-05 19:07:17 -0400

    commit

--- main.js
+++ main.js
@@ -24,21 +24,30 @@ inputs.forEach(function (input) {
     input.addEventListener("change", function (e) {
 
         const this_input = e.target; 
-        const this_value = e.parseFloat(e.target.value);
+        const qty = e.parseFloat(e.target.value);
         const this_row = this_input.closest(".row")
         
 
         const amazon = this_row.querySelector (".amazon");
         const amazon_span = amazon.querySelector("span");
         const amazon_price = parseFloat(amazon.dataset.price);
+        const amazon_cost = qty * amazon_price;
+
+        amazon_span.innerHTML = amazon_cost;
 
         const freshdirect = this_row.querySelector(".freshdirect");
        const freshdirect_span = freshdirect.querySelector("span");
        const freshdirect_price = parseFloat(freshdirect.dataset.price);
+       const freshdirect_cost - qty * freshdirect_price;
+
+       freshdirect_span.innerHTML = freshdirect_cost;
 
         const peapod = this_row.querySelector (".peapod");
         const peapod_span = peapod.querySelector("span");
         const peapod_price = parseFloat(peapod.dataset.price);
+        const peapod_cost = qty * peapod_price;
+
+        peapod_span.innerHTML = peapod_cost;
 
     });
 });
