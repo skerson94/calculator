9d3242eb26b4af9d725881c2fd2468a5cfa12683
Sarah Kerson <sarah.kerson25@journalism.cuny.edu>
2022-04-05 18:54:28 -0400

    commit

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ function round_number(num) {
 
     return num;
 }
+
+//get all of the calculator inputs 
+const inputs = document.querySelectorAll("name='qty']");
+
+//evaluate all of the inputs 
+inputs.forEach(function (input) {
+
+    //for each individual input, listen for change 
+    input.addEventListener("change", function (e) {
+
+        const this_input = e.target; 
+        const this_value = e.parseFloat(e.target.value);
+        const this_row = this_input.closest(".row")
+        
+
+        const amazon = this_row.querySelector (".amazon");
+        const amazon_span = amazon.querySelector("span");
+        const amazon_price = parseFloat(amazon.dataset.price);
+
+        const freshdirect = this_row.querySelector(".freshdirect");
+       const freshdirect_span = freshdirect.querySelector("span");
+       const freshdirect_price = parseFloat(freshdirect.dataset.price);
+
+        const peapod = this_row.querySelector (".peapod");
+        const peapod_span = peapod.querySelector("span");
+        const peapod_price = parseFloat(peapod.dataset.price);
+
+    });
+});
